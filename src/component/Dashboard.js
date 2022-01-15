57ce89ea9036938e7933f6d6e3db35f56a1607bd
jeanlouisHERVE <jeanlouis.herve@hotmail.fr>
2022-01-15 14:53:19 +0100

    add a logout button to the dashboard

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -1,5 +1,37 @@
-
+import { useState } from "react";
+import { Card, Button, Alert } from "react-bootstrap";
+import { useAuth } from '../contexts/AuthContext';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Dashboard () {
-    return <div>Dashboard</div>
+    const [error, setError] = useState('')
+    const { currentUser, logout } = useAuth()
+    const navigate = useNavigate()
+
+    async function handleLogOut() {
+        setError('')
+
+        try {
+            await logout();
+            navigate('/login')
+        } catch {
+            setError('Failed to log out')
+        }
+    }
+
+    return (
+        <>
+            <Card>
+                <Card.Body>
+                    <h2 className='text-center mb-3'>Profile</h2>
+                    {error && <Alert variant="danger">{error}</Alert>}
+                    <strong>Email :</strong>{currentUser.email}
+                    <Link to="/update-profile" className="btn btn-primary w-100 mt-3">Update Profile</Link>
+                </Card.Body>
+            </Card>
+            <div className='w-100 text-center mt-2'>
+                <Button variant="link" onClick={handleLogOut}>Log Out</Button>
+            </div>
+        </>
+        )
 }
