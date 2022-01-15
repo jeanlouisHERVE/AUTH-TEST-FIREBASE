d24c67a745c7e366363dc68f37be3a320e1c7f42
jeanlouisHERVE <jeanlouis.herve@hotmail.fr>
2022-01-15 13:30:11 +0100

    signup Page works

--- src/component/App.js
+++ src/component/App.js
@@ -1,13 +1,16 @@
 import { Container } from "react-bootstrap";
+import { AuthProvider } from "../contexts/AuthContext";
 import Signup from "./signup";
 
 function App() {
   return (
+    <AuthProvider>
       <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh"}}>
        <div className="w-100" style={{ maxWidth: "400px"}}>
         <Signup />
        </div>
       </Container>
+    </AuthProvider>
   );
 }
 
