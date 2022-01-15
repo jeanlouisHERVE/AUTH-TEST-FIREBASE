36070ad9dedbcd1e3ae40b3d9569e3c6748a494c
jeanlouisHERVE <jeanlouis.herve@hotmail.fr>
2022-01-15 14:15:55 +0100

    add of the Log in component and routes with RRD

--- src/component/App.js
+++ src/component/App.js
@@ -1,16 +1,27 @@
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "../contexts/AuthContext";
-import Signup from "./signup";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./Signup";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
 
 function App() {
   return (
-    <AuthProvider>
+    
       <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh"}}>
        <div className="w-100" style={{ maxWidth: "400px"}}>
-        <Signup />
+        <Router>
+          <AuthProvider>
+            <Routes>
+              <Route exact path="/" element={<Dashboard/>}/>
+              <Route path="/signup" element={<Signup/>}/>
+              <Route path="/login" element={<Login/>}/>
+            </Routes>
+          </AuthProvider>
+        </Router>
        </div>
       </Container>
-    </AuthProvider>
+    
   );
 }
 
